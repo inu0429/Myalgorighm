14f71890f1ebf46665cfc9d46407201bf7cd80c7
Inu_java <80373000+inu0429@users.noreply.github.com>
2021-06-09 18:47:20 +0900

    Update Main.java

--- 모의담금질/Main.java
+++ 모의담금질/Main.java
@@ -18,7 +18,7 @@ public class Main {
             public boolean isNeighborBetter(double f0, double f1) {
                 return f1 > f0;
             }
-        }, -4, 8);  // -4~부터 4까지 x값 범위 설정 하기위해서 upper값을 4더 큰 값을 대입.
+        }, -4, 8);  // -4~부터 4까지 x값 범위 설정 하기위해서 upper값을 4더 큰 값을 대입. 그 외에도 원하는 x범위를 설정해주면 된다.
 
         System.out.println(sa.hist);
         // x=-4 f(x) = -64
